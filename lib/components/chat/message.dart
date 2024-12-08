c05eb64ca22e09592016c68135a7dccbf1494785
Antonio Carlos <antoniocarlosjr850@gmail.com>
2024-12-08 00:08:18 -0300

    feat: add Message widget for displaying chat messages with response options

---
+++ lib/components/chat/message.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:laboratorio/components/chat/copy.dart';
+import 'package:laboratorio/components/chat/reproduceText.dart';
+
+const _colorWhite = Color.fromRGBO(255, 252, 255, 1);
+const _colorDark = Color.fromRGBO(80, 81, 79, 1);
+
+class Message extends StatelessWidget {
+  final String text;
+  final bool isReponse;
+
+  const Message({
+    super.key,
+    required this.isReponse,
+    required this.text,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        isReponse ? const SizedBox(height: 16) : const SizedBox.shrink(),
+        Container(
+          decoration: BoxDecoration(
+            color: isReponse ? _colorDark : _colorWhite,
+            borderRadius: BorderRadius.circular(8),
+            boxShadow: const [
+              BoxShadow(
+                color: Colors.black12,
+                blurRadius: 4,
+                offset: Offset(2, 2),
+              ),
+            ],
+          ),
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              Text(
+                text,
+                style: TextStyle(
+                  fontSize: 14,
+                  color: isReponse ? _colorWhite : _colorDark,
+                ),
+              ),
+            ],
+          )
+        ),
+        isReponse ?
+          Row(
+            children: [
+              CopyButton(text: text),
+              Reproducetext(text: text)
+            ],
+          )
+          : const SizedBox.shrink(),
+      ],
+    );
+  }
+}
+
