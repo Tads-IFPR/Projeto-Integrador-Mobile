c7cc853b7025a09b03170f7287b19029377ff47c
Antonio Carlos <antoniocarlosjr850@gmail.com>
2024-12-11 22:48:32 -0300

    feat(chat): convert Reproducetext to StatefulWidget and add audio playback functionality

--- lib/components/chat/reproduceText.dart
+++ lib/components/chat/reproduceText.dart
@@ -1,6 +1,10 @@
-import 'package:flutter/material.dart';
+import 'dart:io';
 
-class Reproducetext extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:flutter_sound/flutter_sound.dart';
+import 'package:laboratorio/main.dart';
+
+class Reproducetext extends StatefulWidget {
   final String text;
 
   const Reproducetext({
@@ -9,10 +13,64 @@ class Reproducetext extends StatelessWidget {
   });
 
   @override
+  State<Reproducetext> createState() => _ReproducetextState();
+}
+
+class _ReproducetextState extends State<Reproducetext> {
+  final FlutterSoundPlayer _player = FlutterSoundPlayer();
+  File? audioFile;
+  File? _currentlyPlayingAudio;
+
+  Future<File?> _textToAudio(String text) async {
+    return await openAIService.textToSpeach(text);
+  }
+
+  Future<void> _toggleAudio() async {
+    audioFile ??= await _textToAudio(widget.text);
+
+    if (_player.isPlaying && _currentlyPlayingAudio == audioFile) {
+      await _player.stopPlayer();
+      setState(() {
+        _currentlyPlayingAudio = null;
+      });
+    } else {
+      if (_player.isPlaying) {
+        await _player.stopPlayer();
+      }
+
+      await _player.startPlayer(
+        fromURI: audioFile?.path,
+        codec: Codec.mp3,
+        whenFinished: () => setState(() {
+          _currentlyPlayingAudio = null;
+        }),
+      );
+
+      setState(() {
+        _currentlyPlayingAudio = audioFile;
+      });
+    }
+  }
+
+  _initializePlayer() async {
+    await _player.openPlayer();
+  }
+
+    @override
+  void initState() {
+    super.initState();
+    _initializePlayer();
+  }
+
+  @override
+  void dispose() {
+    _player.closePlayer();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return IconButton(onPressed: () => {
-        print('Playing: $text')	
-      }, 
+    return IconButton(onPressed: _toggleAudio, 
       icon: const Icon(Icons.graphic_eq)
     );
   }
