d64ef3bfd7dba6785683953762eb66e4100c1e5b
Antonio Carlos <antoniocarlosjr850@gmail.com>
2025-01-12 18:11:18 -0300

    refactor: improve ChatDAO by encapsulating chat retrieval methods and optimizing message management

--- lib/dao/chat.dart
+++ lib/dao/chat.dart
@@ -2,12 +2,8 @@ import 'package:laboratorio/database/database.dart';
 
 class ChatDAO extends AppDatabase {
   Chat? _currentChat;
-  List<Chat> allChats = [];
+  List<Chat> _allChats = [];
   List<Message> _messages = [];
-
-  Future<List<Message>> getMessagesForChat(int chatId) {
-    return (select(db.messages)..where((tbl) => tbl.chatId.equals(chatId))).get();
-  }
 
   set currentChat(Chat? chat) {
     setChat(chat!);
@@ -21,12 +17,34 @@ class ChatDAO extends AppDatabase {
 
   Chat? get currentChat => _currentChat;
 
-  Future<List<Message>> get curretChatMessagesmessages async {
+  Future<List<Message>> getMessagesForChat(int chatId) {
+    return (select(db.messages)..where((tbl) => tbl.chatId.equals(chatId))).get();
+  }
+
+  Future<List<Chat>> getAllChats() async {
+    if (_allChats.isNotEmpty) return _allChats;
+
+    _allChats = await getAllRecords(chats);
+
+    return _allChats;
+  }
+
+  List<Chat> get allChats {
+    getAllChats();
+    return _allChats;
+  }
+
+  List<Message> get chatMessages {
+    getChatMessage();
+    return _messages;
+  }
+
+  Future<List<Message>> getChatMessage() async {
     if (currentChat == null) return [];
 
     if (_messages.isNotEmpty) return _messages;
 
-    _messages = await (select(db.messages)..where((tbl) => tbl.chatId.equals(currentChat!.id))).get();
+    _messages = await getMessagesForChat(currentChat!.id);
 
     return _messages;
   }
