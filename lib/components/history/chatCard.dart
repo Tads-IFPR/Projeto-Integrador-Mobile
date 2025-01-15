df4567b73ab082e54b9475220cdf32ce88b24817
Antonio Carlos <antoniocarlosjr850@gmail.com>
2025-01-14 21:53:13 -0300

    feat: add ChatCard widget for displaying chat messages

---
+++ lib/components/history/chatCard.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:laboratorio/styles/default.dart';
+
+class ChatCard extends StatelessWidget {
+  final String text;
+  final bool isFirst;
+
+  const ChatCard({
+    super.key,
+    required this.text,
+    required this.isFirst,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        isFirst ? const SizedBox.shrink() : const SizedBox(height: 16),
+        Container(
+          alignment: Alignment.centerLeft,
+          decoration: BoxDecoration(
+            color: colorDark,
+            borderRadius: BorderRadius.circular(8),
+            boxShadow: const [
+              BoxShadow(
+                color: Colors.black12,
+                blurRadius: 4,
+                offset: Offset(2, 2),
+              ),
+            ],
+          ),
+          padding: const EdgeInsets.all(16.0),
+          child: Text(
+            text,
+            style: const TextStyle(
+              fontSize: 14,
+              color: colorWhite
+            ),
+          ) 
+        ),
+      ],
+    );
+  }
+}
