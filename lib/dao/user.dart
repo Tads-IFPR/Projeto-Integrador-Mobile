af54590329582cf8a05f32582f1d3f5dc2bbc12b
Antonio Carlos <antoniocarlosjr850@gmail.com>
2025-01-14 21:32:21 -0300

    feat: implement UserDAO for managing current user state

---
+++ lib/dao/user.dart
@@ -0,0 +1,17 @@
+import 'package:laboratorio/database/database.dart';
+
+class UserDAO extends AppDatabase {
+  User? _currentUser;
+
+  set currentUser(User? user) {
+    setChat(user!);
+  }
+
+  Future<void> setChat(User user) async {
+    _currentUser = user;
+  }
+
+  User? get currentUser => _currentUser;
+}
+
+UserDAO chatDAO = UserDAO();
