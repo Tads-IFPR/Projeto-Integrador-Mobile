6615841d91c2a01cae0575ecaf08de60c63c5edd
LRJonas <jonas.robertinho@gmail.com>
2025-01-25 20:04:53 -0300

    titulo de icone não selecionado corrigido

--- lib/components/bottomNavigator.dart
+++ lib/components/bottomNavigator.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 const _colorWhite = Color.fromRGBO(255, 252, 255, 1);
 const _colorPrimary = Color.fromRGBO(36, 123, 160, 1);
 const _colorDark = Color.fromRGBO(80, 81, 79, 1);
-const _colorUnselected = Color.fromRGBO(150, 150, 150, 1); // Define a color for unselected items
+const _colorUnselected = Color.fromRGBO(150, 150, 150, 1);
 
 class BottomNavigator extends StatefulWidget {
   final ValueChanged<int>? onItemTapped;
@@ -26,9 +26,10 @@ class BottomNavigatorState extends State<BottomNavigator> {
       onTap: widget.onItemTapped,
       backgroundColor: _colorWhite,
       selectedItemColor: _colorDark,
-      unselectedItemColor: _colorUnselected, // Set the color for unselected items
+      unselectedItemColor: _colorUnselected,
       currentIndex: widget.selectedIndex,
       selectedIconTheme: const IconThemeData(color: _colorPrimary),
+      showUnselectedLabels: true,
       items: const [
         BottomNavigationBarItem(
           icon: Icon(Icons.chat),
