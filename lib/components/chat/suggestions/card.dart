35388a221f50f1adb64e6195c4e977669f7859a8
Antonio Carlos <antoniocarlosjr850@gmail.com>
2025-01-28 00:14:08 -0300

    refactor(CardSuggestion): enhance card layout with improved styling and color parameters

--- lib/components/chat/suggestions/card.dart
+++ lib/components/chat/suggestions/card.dart
@@ -1,41 +1,46 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
-import 'package:laboratorio/components/chat/player.dart';
 import 'package:laboratorio/styles/default.dart';
 
 class CardSuggestion extends StatelessWidget {
   final String? text;
   final String? title;
+  final Color? color;
+  final Color? lightColor;
 
   const CardSuggestion({
     super.key,
     required this.title,
     required this.text,
+    required this.color,
+    required this.lightColor,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(16.0),
-      decoration: BoxDecoration(
-        color: colorPrimary,
+    return Card(
+      elevation: 4,
+      shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(8),
-        boxShadow: const [
-          BoxShadow(
-            color: Colors.black12,
-            blurRadius: 4,
-            offset: Offset(2, 2),
-          ),
-        ],
+        side: BorderSide(color: color ?? colorDark, width: 2),
       ),
-      child: Text(
-        text ?? "",
-        style: const TextStyle(
-          fontSize: 14,
-          color: colorWhite,
+      color: lightColor,
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              title ?? "",
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: color),
+            ),
+            const SizedBox(height: 8),
+            Text(
+              text ?? "",
+              style: TextStyle(color: color),
+            ),
+          ],
         ),
-      )
+      ),
     );
   }
 }
